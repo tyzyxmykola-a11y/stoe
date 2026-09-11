fb85651006c2dd442d7da1b3d0e4d5462b3b4c77
tyzyxmykola-a11y <tyzyxmykola@gmail.com>
2026-09-11 13:14:53 +0300

    Use shared verification policy in StoeCoder runtime

--- StoeCoder/server.py
+++ StoeCoder/server.py
@@ -7,6 +7,7 @@ reusing its current UI/API surface from the StoeCoder project directory.
 from ui_server import app, coder, _coder_local_request
 from event_logging import install_event_logging
 from anti_loop import install_anti_loop
+from verification_policy import install_verification_policy
 from workflow_guard import install_workflow_guard
 from workflow_controls import install_workflow_controls
 from diagnostic_logging import install_diagnostic_logging
@@ -14,6 +15,7 @@ from model_io import install_model_io_capture, install_model_io_ui
 
 install_event_logging(coder)
 install_anti_loop(coder)
+install_verification_policy(coder)
 install_workflow_guard(coder)
 install_workflow_controls(coder)
 install_diagnostic_logging(coder)

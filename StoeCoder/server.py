ae85fceaab9b3f91c0e1e3a5c062069270738b09
tyzyxmykola-a11y <tyzyxmykola@gmail.com>
2026-09-11 15:47:06 +0300

    Wire anchored inspection navigation

--- StoeCoder/server.py
+++ StoeCoder/server.py
@@ -7,6 +7,7 @@ reusing its current UI/API surface from the StoeCoder project directory.
 from ui_server import app, coder, _coder_local_request
 from event_logging import install_event_logging
 from repository_navigation import install_repository_navigation, install_information_gain_tracking
+from inspection_navigation import install_inspection_navigation
 from anti_loop import install_anti_loop
 from verification_policy import install_verification_policy
 from workflow_guard import install_workflow_guard
@@ -16,6 +17,7 @@ from model_io import install_model_io_capture, install_model_io_ui
 
 install_event_logging(coder)
 install_repository_navigation(coder)
+install_inspection_navigation(coder)
 install_anti_loop(coder)
 install_information_gain_tracking(coder)
 install_verification_policy(coder)

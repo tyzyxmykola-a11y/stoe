62813361019d32561ed355d878f6dcf1e638ffaa
tyzyxmykola-a11y <tyzyxmykola@gmail.com>
2026-09-11 15:08:10 +0300

    Wire compact repository navigation

--- StoeCoder/server.py
+++ StoeCoder/server.py
@@ -6,6 +6,7 @@ reusing its current UI/API surface from the StoeCoder project directory.
 
 from ui_server import app, coder, _coder_local_request
 from event_logging import install_event_logging
+from repository_navigation import install_repository_navigation, install_information_gain_tracking
 from anti_loop import install_anti_loop
 from verification_policy import install_verification_policy
 from workflow_guard import install_workflow_guard
@@ -14,7 +15,9 @@ from diagnostic_logging import install_diagnostic_logging
 from model_io import install_model_io_capture, install_model_io_ui
 
 install_event_logging(coder)
+install_repository_navigation(coder)
 install_anti_loop(coder)
+install_information_gain_tracking(coder)
 install_verification_policy(coder)
 install_workflow_guard(coder)
 install_workflow_controls(coder)
